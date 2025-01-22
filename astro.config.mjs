f28807bb20a89f425869938722492fe4fda515af
yussef432 <124080156+yussef432@users.noreply.github.com>
2025-01-22 20:14:45 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,36 +1,13 @@
-import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
-import netlify from "@astrojs/netlify";
-import robotsTxt from "astro-robots-txt";
-import UnoCSS from "@unocss/astro";
+import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
 import icon from "astro-icon";
+import { defineConfig } from 'astro/config';
 
-import solidJs from "@astrojs/solid-js";
-import { remarkReadingTime } from "./src/lib/remark-reading-time.mjs";
-
-import svelte from "@astrojs/svelte";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://yussef432.github.io/",
-  integrations: [
-    sitemap(),
-    robotsTxt({
-      sitemap: [
-        "https://yussef432.github.io/sitemap-index.xml",
-        "https://yussef432.github.io/sitemap-0.xml",
-      ],
-    }),
-    solidJs(),
-    UnoCSS({ injectReset: true }),
-    icon(),
-    svelte(),
-  ],
-  markdown: {
-    remarkPlugins: [remarkReadingTime],
-  },
-  adapter: netlify({ edgeMiddleware: true }),
-  vite: {
-    assetsInclude: "**/*.riv",
-  },
+  integrations: [tailwind(), icon(), compress()],
+  adapter: vercel()
 });
